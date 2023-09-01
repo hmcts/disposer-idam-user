56f8015c0333f8bb3471506e08cdbda64699b14d
Priyanka Verma <priyanka.verma@hmcts.net>
2023-09-01 16:31:29 +0100

    LAU-710 functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/idam/controllers/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/idam/controllers/SampleFunctionalTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.idam.controllers;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Slf4j
 class SampleFunctionalTest {
     protected static final String CONTENT_TYPE_VALUE = "application/json";
 
     @Value("${TEST_URL:http://localhost:8080}")
     private String testUrl;
+
+    @Value("$DISPOSER_IDAM_USER_CLIENT_SECRET")
+    private String client_secret;
+
+    @Value("$S2S_SECRET_DISPOSER_IDAM_USER")
+    private String s2sSecret;
 
     @BeforeEach
     public void setUp() {
@@ -35,5 +43,7 @@ class SampleFunctionalTest {
 
         Assertions.assertEquals(200, response.statusCode());
         Assertions.assertTrue(response.asString().startsWith("Welcome"));
+        log.info("IDAM_USER_CLIENT_SECRET : " + client_secret);
+        log.info("S2S_SECRET_DISPOSER_IDAM_USER : " + s2sSecret);
     }
 }
