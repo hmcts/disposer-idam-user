ed08ac9affa034c6d40fef0c819d617381d999ab
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-07-14 09:23:53 +0100

    remove not needed suppressions

--- src/integrationTest/java/uk/gov/hmcts/reform/idam/bdd/CucumberTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/idam/bdd/CucumberTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
     features = "classpath:features",
     plugin = {"pretty", "html:target/cucumber/cucumber-report.html"},
     monochrome = true)
-@SuppressWarnings({"PMD.TestClassWithoutTestCases"})
 public class CucumberTest {
 
 }
