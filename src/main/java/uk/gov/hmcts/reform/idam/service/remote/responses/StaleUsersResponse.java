bd40800a7cd9c87c5de775ef2bf0a7c892ff70fb
Rapolas <130578328+rapolaskaseliscgi@users.noreply.github.com>
2025-10-01 10:31:57 +0100

    LAU-1275 remove total pages log message

--- src/main/java/uk/gov/hmcts/reform/idam/service/remote/responses/StaleUsersResponse.java
+++ src/main/java/uk/gov/hmcts/reform/idam/service/remote/responses/StaleUsersResponse.java
@@ -1,25 +1,14 @@
 package uk.gov.hmcts.reform.idam.service.remote.responses;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class StaleUsersResponse {
+public record StaleUsersResponse(List<UserContent> content, boolean last) {
 
-    @JsonProperty("content")
-    private List<UserContent> content;
+    public List<UserContent> getContent() {
+        return content;
+    }
 
-    @JsonProperty("last")
-    private Boolean isLast;
-
-    @JsonProperty
-    private Long totalPages;
+    public boolean isLast() {
+        return last;
+    }
 }
